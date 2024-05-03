7c2ee4217ec8ef5ac1fa5188ea3f347e7afe0340
Sofiosu24 <A01277695@tec.mx>
2024-05-03 16:34:15 -0600

    Actualizar laboratorio 18 y agregar laboratorio 19

--- Lab19/models/usuarios.model.js
+++ Lab19/models/usuarios.model.js
@@ -1,7 +1,6 @@
 const db     = require('../utils/database.js');
 const bcrypt = require('bcryptjs');
 
-
 exports.login = function(correo,contrasena){
     return {
         nombre:"Samuel",
@@ -10,7 +9,7 @@ exports.login = function(correo,contrasena){
     };
 }
 
-exports.User = class  {
+exports.User = class {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(my_username, my_name, my_password) {
         this.username = my_username;
@@ -22,10 +21,16 @@ exports.User = class  {
         try {
             const connection = await db();
             const hashedPass = await bcrypt.hash(this.password, 12)
-            const result = await connection.execute(
-            `INSERT INTO users (username, name, password) VALUES (?, ?, ?)`,
-            [this.username, this.name, hashedPass]
+            const usuario = await connection.execute(
+                `INSERT INTO usuario (username, nombre, password) VALUES (?, ?, ?)`,
+                [this.username, this.name, hashedPass]
             );
+    
+            const result = await connection.execute(
+                'INSERT INTO asigna (username, idrol) VALUES (?, 1)',
+                [this.username]
+            );
+    
             await connection.release();
             return result;
         } catch (error) {
@@ -33,11 +38,22 @@ exports.User = class  {
         }
     }
     //Este método servirá para buscar un usuario por username
-    //Es estático ya que a diferencia de save(), el primero se guarda al crear un usuario siempre, pero en este segundo podmeos buscar un usuario sin crear un nuevo objeto usuario.
     static async findUser(username) {
         try {
             const connection = await db();
-            const result = await connection.execute('Select * from users WHERE username = ?', [username]);
+            const result = await connection.execute('Select * from usuario WHERE username = ?', [username]);
+            await connection.release();
+            return result;
+        } catch (error) {
+            throw error; // Re-throw the error for proper handling
+        }
+    }
+
+    //Este método servirá para obtener los permisos de un usuario
+    static async getPermisos(username) {
+        try {
+            const connection = await db();
+            const result = await connection.execute('Select permiso FROM privilegio pr, posee po, rol r, asigna a, usuario u WHERE u.username = ? AND u.username = a.username AND a.idrol = r.id AND r.id = po.idrol AND po.idpermiso = pr.id', [username]);
             await connection.release();
             return result;
         } catch (error) {
